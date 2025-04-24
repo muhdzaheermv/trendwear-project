2f870ab81cfb7db0c3c61e96d94c411ec9aa0c3f
zaheer mv <muhdzaheermv@gmail.com>
2025-04-24 17:37:32 +0530

    update settings.py

--- hashiq/settings.py
+++ hashiq/settings.py
@@ -15,7 +15,6 @@ import os
 import dj_database_url
 from decouple import config
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -129,8 +128,31 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 STATICFILE_DIR=[os.path.join(BASE_DIR,'static')]
 
-
+RAZOR_KEY_ID ='rzp_test_X5OfG2jiWrAzSj'
+RAZOR_KEY_SECRET ='SsCovWWZSwB1TGd1rSoIiwF3'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+RAPID_API_KEY="d1df21eb15mshd4dd04de7198e11p16e1dejsnb1da9edd5cf8"
+
+#cloudinary api
+CLOUDINARY_API_KEY="713126228575184"
+CLOUDINARY_CLOUD_NAME="dbd4gn5ms"
+CLOUDINARY_API_SECRET="lECYpdCyoO53vYt5NGVOugbJ05s"
+
+#sepapi api
+
+SEPAPI_KEY="16afa4e012ff315bb0faac7b6708ea6191302f932604b4454084506712205d53"
+
+EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER= 'nefsal003@gmail.com'
+EMAIL_HOST_PASSWORD= 'htxalvzrrkxupspv'
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
